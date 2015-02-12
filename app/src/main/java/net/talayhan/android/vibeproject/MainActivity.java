fb2f3532f058ef213050a59af183bf3f554ea96a
Samet Sait Talayhan <samet.talayhan@gmail.com>
2015-02-12 19:09:56 +0200

    [+] FileChooser works.

--- app/src/main/java/net/talayhan/android/vibeproject/MainActivity.java
+++ app/src/main/java/net/talayhan/android/vibeproject/MainActivity.java
@@ -1,11 +1,18 @@
 package net.talayhan.android.vibeproject;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import com.ipaulpro.afilechooser.utils.FileUtils;
+
+import java.io.File;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -23,11 +30,35 @@ public class MainActivity extends ActionBarActivity {
         mFileChooser_bt.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                // Create the ACTION_GET_CONTENT Intent
+                Intent getContentIntent = FileUtils.createGetContentIntent();
 
+                Intent intent = Intent.createChooser(getContentIntent, "Select a file");
+                startActivityForResult(intent, Constants.REQUEST_CHOOSER);
             }
         });
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch (requestCode) {
+            case Constants.REQUEST_CHOOSER:
+                if (resultCode == RESULT_OK) {
+
+                    final Uri uri = data.getData();
+
+                    // Get the File path from the Uri
+                    String path = FileUtils.getPath(this, uri);
+                    Toast.makeText(this, "Choosen file: " + path,Toast.LENGTH_LONG).show();
+
+                    // Alternatively, use FileUtils.getFile(Context, Uri)
+                    if (path != null && FileUtils.isLocal(path)) {
+                        File file = new File(path);
+                    }
+                }
+                break;
+        }
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
