5d62989ad5c2ba3b540466e53cd9fb6b94b256ab
Samet Sait Talayhan <samet.talayhan@gmail.com>
2015-02-23 10:47:49 +0200

    Added spinner on XML.

--- app/src/main/java/net/talayhan/android/vibeproject/LocalVideoActivity.java
+++ app/src/main/java/net/talayhan/android/vibeproject/LocalVideoActivity.java
@@ -36,77 +36,20 @@ public class LocalVideoActivity extends Activity {
         
         /* Video settings */
         mVideoView_vw = (VideoView) findViewById(R.id.videoView_vw);
+        vidAddress = getIntent().getStringExtra(Constants.EXTRA_ANSWER_IS_TRUE);
+        Toast.makeText(LocalVideoActivity.this,vidAddress, Toast.LENGTH_LONG).show();
+
+        vidUri = Uri.parse(vidAddress);
+
+        /* Custom video settings for internet */
+        mVideoView_vw.setVideoURI(vidUri);
+        mVideoView_vw.setMediaController(new MediaController(LocalVideoActivity.this));
+        mVideoView_vw.start();
         
-        if(firstLooking == true) {
-            firstLooking = false;   // Now anymore, first time
-            /* Put here AlertDialog to make a decision between  */
-            new AlertDialog.Builder(this)
-                    .setTitle("Internet or Local?")
-                    .setMessage("Would you like to watch the video on the internet or local storage?")
-                    .setPositiveButton("Internet", new DialogInterface.OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface dialog, int which) {
-                            // Internet
-                            /* define string to test VideoView component */
-                            vidAddress = "https://archive.org/download/ksnn_compilation_master_the_internet/ksnn_compilation_master_the_internet_512kb.mp4";
-                            vidUri = Uri.parse(vidAddress);
-                            
-                            /* Custom video settings for internet */
-                            mVideoView_vw.setVideoURI(vidUri);
-                            mVideoView_vw.setMediaController(new MediaController(LocalVideoActivity.this));
-                            mVideoView_vw.start();
-                            
-                            /* Start transparent activity */
-                            Intent intent = new Intent(LocalVideoActivity.this, DrawingActivity.class);
-                            startActivityForResult(intent, Constants.REQUEST_CHOOSER);
-                        }
-                    })
-                    .setNegativeButton("Local", new DialogInterface.OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface dialog, int which) {
-                            // Local
-                            // Create the ACTION_GET_CONTENT Intent
-                            Intent getContentIntent = FileUtils.createGetContentIntent();
+        Intent transparentLayoutInt = new Intent(LocalVideoActivity.this, DrawingActivity.class);
+        startActivityForResult(transparentLayoutInt,Constants.REQUEST_CHOOSER);
+        
+        
 
-                            Intent intent = Intent.createChooser(getContentIntent, "Select a file");
-                            startActivityForResult(intent, Constants.REQUEST_CHOOSER);
-
-                        }
-                    })
-                    .setIcon(android.R.drawable.ic_dialog_alert)
-                    .show();
-        }
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        switch (requestCode) {
-            case Constants.REQUEST_CHOOSER:
-                if (resultCode == RESULT_OK) {
-
-                    final Uri uri = data.getData();
-
-                    // Get the File path from the Uri
-                    vidAddress = FileUtils.getPath(this, uri);
-                    /* define string to test VideoView component */
-                    vidUri = Uri.parse(vidAddress);
-                    Toast.makeText(this, "Choosen file: " + vidAddress,Toast.LENGTH_LONG).show();
-                    
-                    /* Custom video settings for internet */
-                    mVideoView_vw.setVideoURI(vidUri);
-                    mVideoView_vw.setMediaController(new MediaController(LocalVideoActivity.this));
-                    mVideoView_vw.start();
-
-                    /* Start transparent activity */
-                    Intent intent2 = new Intent(LocalVideoActivity.this, DrawingActivity.class);
-                    startActivityForResult(intent2, Constants.REQUEST_CHOOSER);
-
-                    // Alternatively, use FileUtils.getFile(Context, Uri)
-                    if (vidAddress != null && FileUtils.isLocal(vidAddress)) {
-                        File file = new File(vidAddress);
-                    }
-                }
-                break;
-        }
     }
 }
--- app/src/main/java/net/talayhan/android/vibeproject/MainActivity.java
+++ app/src/main/java/net/talayhan/android/vibeproject/MainActivity.java
@@ -1,5 +1,7 @@
 package net.talayhan.android.vibeproject;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.Uri;
 import android.support.v7.app.ActionBarActivity;
@@ -8,6 +10,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.MediaController;
 import android.widget.Toast;
 
 import com.ipaulpro.afilechooser.utils.FileUtils;
@@ -20,13 +23,54 @@ public class MainActivity extends ActionBarActivity {
     /* private members */
     private Button mFileChooser_bt;
     private Button mVibe_bt;
+    private String videoPath;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        /* initialize file chooser button */
+
+        new AlertDialog.Builder(this)
+                .setTitle("Internet or Local?")
+                .setMessage("Would you like to watch the video on the internet or local storage?")
+                .setPositiveButton("Internet", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        
+                        // Internet
+                            /* define string to test VideoView component *
+                        vidAddress = "https://archive.org/download/ksnn_compilation_master_the_internet/ksnn_compilation_master_the_internet_512kb.mp4";
+                        vidUri = Uri.parse(vidAddress);
+                            
+                            /* Custom video settings for internet *
+                        mVideoView_vw.setVideoURI(vidUri);
+                        mVideoView_vw.setMediaController(new MediaController(LocalVideoActivity.this));
+                        mVideoView_vw.start();
+                            
+                            /* Start transparent activity *
+                        Intent intent = new Intent(LocalVideoActivity.this, DrawingActivity.class);
+                        startActivityForResult(intent, Constants.REQUEST_CHOOSER);
+                        
+                        */
+                    }
+                })
+                .setNegativeButton("Local", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        // Local
+                        // Create the ACTION_GET_CONTENT Intent
+                        Intent getContentIntent = FileUtils.createGetContentIntent();
+
+                        Intent intent = Intent.createChooser(getContentIntent, "Select a file");
+                        startActivityForResult(intent, Constants.REQUEST_CHOOSER);
+
+                    }
+                })
+                .setIcon(android.R.drawable.ic_dialog_alert)
+                .show();
+
+        /* initialize file chooser button *
         mFileChooser_bt = (Button) findViewById(R.id.fileChooser_bt);
         mFileChooser_bt.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -39,7 +83,7 @@ public class MainActivity extends ActionBarActivity {
             }
         });
         
-        /* initialize video test button */
+        /* initialize video test button *
         mVibe_bt = (Button) findViewById(R.id.vibeTest_bt);
         mVibe_bt.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -47,9 +91,10 @@ public class MainActivity extends ActionBarActivity {
                 // Create the intent to start video activity
                 Intent i = new Intent(MainActivity.this, LocalVideoActivity.class);
                 startActivity(i);
+                
             }
         });
-
+        */
     }
 
     @Override
@@ -62,12 +107,20 @@ public class MainActivity extends ActionBarActivity {
 
                     // Get the File path from the Uri
                     String path = FileUtils.getPath(this, uri);
+                    
                     Toast.makeText(this, "Choosen file: " + path,Toast.LENGTH_LONG).show();
 
                     // Alternatively, use FileUtils.getFile(Context, Uri)
                     if (path != null && FileUtils.isLocal(path)) {
                         File file = new File(path);
                     }
+
+                    // Create the intent to start video activity
+                    Intent i = new Intent(MainActivity.this, LocalVideoActivity.class);
+                    i.putExtra(Constants.EXTRA_ANSWER_IS_TRUE,path);
+                    startActivity(i);
+
+
                 }
                 break;
         }
